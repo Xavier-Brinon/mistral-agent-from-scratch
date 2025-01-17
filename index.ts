f383c947c31cd1762eb13cd8e78256aed187889a
Xavier-Brinon <xavier@brinon.eu>
2025-01-17 12:07:14 +0000

    feat: create a request to a mistral model

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
-const message: string = 'this is a message'
-console.debug(message)
+import { Mistral } from '@mistralai/mistralai'
+import { mistralModels } from './models.ts'
+
+const apiKey = process.env.MISTRAL_API_KEY
+const client = new Mistral({ apiKey })
+
+const chatResponse = await client.chat.complete({
+  model: mistralModels.MistralNemo,
+  messages: [
+    {
+      role: 'user',
+      content: 'What is the best french cheese?'
+    }
+  ]
+})
+
+console.debug({ chatResponse: JSON.stringify(chatResponse, null, 2) })
+console.log(chatResponse?.choices?.[0].message.content)
+
---
+++ models.ts
@@ -0,0 +1,6 @@
+export const mistralModels = {
+  MistralLarge: 'mistral-large-latest',
+  Pixtral: 'pixtral-12b-2409',
+  MistralNemo: 'open-mistral-nemo',
+  CodestralMamba: 'open-codestral-mamba',
+} as const
